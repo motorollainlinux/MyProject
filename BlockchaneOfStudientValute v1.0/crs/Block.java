cf389dc322aa93a6a59ae333eb7217b987b44b40
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-01-19 19:59:23 +0700

    java classes

---
+++ BlockchaneOfStudientValute v1.0/crs/Block.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Block {
+    int Index, Hash, PrevHash;
+    String Key, data;
+    Date date;
+    public Block(int prevHash, int index, Date date, String data, int hash, String key) {
+        this.PrevHash = PrevHash;
+        this.Index = Index;
+        this.date = this.date;
+        this.data = this.data;
+        this.Hash = Hash;
+        this.Key = Key;
+    }
+}
